d8dd61fe3df28bb3879fab4ab7bc6e597e83cadc
Brendan Anderson <brendan_anderson@hcpss.org>
2018-03-06 12:07:22 -0500

    Chown image dirs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,7 @@
 
 chgrp -R www-data /var/lib/php/sessions
 chown -R www-data:www-data /var/www/symfony/var
+chown -R www-data:www-data /var/www/symfony/public/images
+chown -R www-data:www-data /var/www/symfony/public/imagine
 
 exec "$@"
