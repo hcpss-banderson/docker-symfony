e7a12e53f7d42de98c38c753a5124414ea823733
Brendan Anderson <brendan_anderson@hcpss.org>
2018-07-02 13:47:55 -0400

    Remove console scripts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-/var/www/symfony/bin/console cache:clear
-/var/www/symfony/bin/console assets:install --symlink --relative %PUBLIC_DIR%
-
 chgrp -R www-data /var/lib/php/sessions
 chown -R www-data:www-data /var/www/symfony/var
 chown -R www-data:www-data /var/www/symfony/public/images
