e0138ddaa816f5bdd96ccc2d169667165b353138
Brendan Anderson <brendan_anderson@hcpss.org>
2018-07-02 12:10:53 -0400

    clear cache on run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+/var/www/symfony/bin/console cache:clear
+/var/www/symfony/bin/console assets:install --symlink --relative %PUBLIC_DIR%
+
 chgrp -R www-data /var/lib/php/sessions
 chown -R www-data:www-data /var/www/symfony/var
 chown -R www-data:www-data /var/www/symfony/public/images
